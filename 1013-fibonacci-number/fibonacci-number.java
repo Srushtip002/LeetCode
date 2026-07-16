f306d9e96a142b3142722831950d7a93461f9734
Srushtip002 <shrush.s.patil@gmail.com>
2026-07-16 22:54:55 +0530

    Time: 10 ms (26.78%) | Memory: 42.1 MB (33.36%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+        return fibHelper(n);
+    }
+
+    private int fibHelper(int n) {
+        if (n == 0)
+            return 0;
+
+        if (n == 1)
+            return 1;
+
+        int prev = fibHelper(n - 1);
+        int prePrev = fibHelper(n - 2);
+
+        return prev + prePrev;
+    }
+}
